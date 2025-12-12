e9eee78eaf235fd5d371324f3c527ee14803576d
Camila Lima <milafmhel@gmail.com>
2025-12-12 14:35:25 -0300

    Troquei a fórmula do teorema de pitágoras por uma função do módulo math.

--- modulo1/calculadora_triangulos/calculadora_hipotenusa.py
+++ modulo1/calculadora_triangulos/calculadora_hipotenusa.py
@@ -1,11 +1,13 @@
 import math
 
-print('Bem vindo! Este programa calcula a hipotenusa de um triangulo retangulo.\nPara descobrir a hipotenusa, eu preciso do cateto oposto e do cateto adjacente, que você sabe quais são.')
-CtOposto = int(input('Primeiro, me diga o cateto oposto: '))
-CtAdjacente = int(input('e agora, o cateto adjacente: '))
+print('Bem vindo! Este programa calcula a hipotenusa de um triangulo retangulo.\nPara descobrir a hipotenusa, eu preciso do cateto oposto e do cateto adjacente.')
+CtOposto = float(input('Primeiro, me diga o cateto oposto: '))
+CtAdjacente = float(input('e agora, o cateto adjacente: '))
 
 #calculo 
 
-conta = math.sqrt( CtOposto ** 2 + CtAdjacente ** 2 )
+conta = math.hypot(CtOposto, CtAdjacente)
 
-print(f'A hipotenusa é {int(conta)}')
+#usando a fórmula: conta = math.sqrt( CtOposto ** 2 + CtAdjacente ** 2 )
+
+print(f'A hipotenusa é {conta}')
